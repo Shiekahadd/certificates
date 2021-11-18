885342e7f9d42c6aa32efcf707d82a802181ecc7
Shiekahadd <ali19003@byui.edu>
2021-11-18 15:56:26 -0700

    rexburg weather

--- W10 Team Activity/rexburg.js
+++ W10 Team Activity/rexburg.js
@@ -1,15 +1,15 @@
-const url =
-   "https://api.openweathermap.org/data/2.5/forecast?id=5605242&units=imperial&appid=744fe90a723fbf1d7ad8053159c6a1d5";
-
-const dofW = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
 var today = new Date();
 var time = today.getHours();
 console.log(time);
-if(time <= 6 && time >= 18){document.body.style.backgroundImage = "url('images/3.png')"}
+if(time <= 6 && time >= 18){document.body.style.backgroundImage = "url('3.jpg')"}
 else {
-   document.body.style.backgroundImage = "url('images/4.png')"
+   document.body.style.backgroundImage = "url('4.jpg')"
 }
+
+const url =
+   "https://api.openweathermap.org/data/2.5/forecast?id=5605242&units=imperial&appid=744fe90a723fbf1d7ad8053159c6a1d5";
+
+const dofW = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]; 
 
 fetch(url)
 
@@ -21,6 +21,37 @@ fetch(url)
       fiveDay(weatherObjct);
    });
 
+   function fiveDay(weatherObjct) {
+      const forecast = weatherObjct.list;
+      const size = forecast.length;
+      const fiveDayElem = document.querySelector('#fiveDay');
+      let fiveDayList = [...fiveDayElem.children];
+  
+      let i2 = 0;
+      for (let i = 0; i < size; i++) {
+          if (forecast[i].dt_txt.includes('15:00:00')) {
+              let highTempElem = document.createElement('p');
+              highTempElem.textContent = `High Temp: ${forecast[i].main.temp_max} °F`;
+  
+              let fiveWeather = document.createElement('img');
+              let imagesrc = "https://openweathermap.org/img/w/" + forecast[i].weather[0].icon + ".png";
+              fiveWeather.setAttribute("src", imagesrc)
+              fiveWeather.setAttribute("alt", forecast[i].weather[0].description)
+              
+              let dt = new Date(forecast[i].dt_txt);
+              let dayElem = document.createElement('h3');
+              dayElem.textContent = dofW[dt.getDay()];
+              
+              
+              fiveDayList[i2].appendChild(dayElem);
+              fiveDayList[i2].appendChild(highTempElem);
+              fiveDayList[i2].appendChild(fiveWeather);
+  
+              i2++;
+          }
+      }
+  }
+
 function cDay(weatherObjct) {
    let weatherData = weatherObjct.list[0];
 
@@ -30,7 +61,7 @@ function cDay(weatherObjct) {
    
 
    let cTemp = document.createElement("p");
-   cTemp.textContent = `Current Temprature: ${weatherData.main.temp} F`;
+   cTemp.textContent = `Air Temprature: ${weatherData.main.temp} °F`;
 
    let cWeather = document.createElement("img");
    let imagesrc =
@@ -41,13 +72,13 @@ function cDay(weatherObjct) {
    cWeather.setAttribute("alt", weatherData.weather[0].description);
 
    let cFeels = document.createElement("p");
-   cFeels.textContent = `Feels like: ${weatherData.main.feels_like}`;
+   cFeels.textContent = `Feels-like: ${weatherData.main.feels_like} °F`;
 
    let tempHigh = document.createElement("p");
-   tempHigh.textContent = `Todays High: ${weatherData.main.temp_max}`;
+   tempHigh.textContent = `Maximum Temperature: ${weatherData.main.temp_max} °F`;
 
    let tempLow = document.createElement("p");
-   tempLow.textContent = `Todays Low: ${weatherData.main.temp_min}`;
+   tempLow.textContent = `Minimum Temperature: ${weatherData.main.temp_min} °F`;
 
    let todayElem = document.querySelector("#today");
 
@@ -60,33 +91,3 @@ function cDay(weatherObjct) {
 
 }
 
-function fiveDay(weatherObjct) {
-    const forecast = weatherObjct.list;
-    const size = forecast.length;
-    const fiveDayElem = document.querySelector('#fiveDay');
-    let fiveDayList = [...fiveDayElem.children];
-
-    let i2 = 0;
-    for (let i = 0; i < size; i++) {
-        if (forecast[i].dt_txt.includes('15:00:00')) {
-            let highTempElem = document.createElement('p');
-            highTempElem.textContent = `High Temp: ${forecast[i].main.temp_max}`;
-
-            let fiveWeather = document.createElement('img');
-            let imagesrc = "https://openweathermap.org/img/w/" + forecast[i].weather[0].icon + ".png";
-            fiveWeather.setAttribute("src", imagesrc)
-            fiveWeather.setAttribute("alt", forecast[i].weather[0].description)
-            
-            let dt = new Date(forecast[i].dt_txt);
-            let dayElem = document.createElement('h3');
-            dayElem.textContent = dofW[dt.getDay()];
-            
-            
-            fiveDayList[i2].appendChild(dayElem);
-            fiveDayList[i2].appendChild(highTempElem);
-            fiveDayList[i2].appendChild(fiveWeather);
-
-            i2++;
-        }
-    }
-}
