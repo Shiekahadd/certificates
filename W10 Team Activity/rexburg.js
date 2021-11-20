0018cb76ed9e658fce5cd4e6b13bba2588945f44
Shiekahadd <ali19003@byui.edu>
2021-11-20 00:05:16 -0700

    final

--- W10 Team Activity/rexburg.js
+++ W10 Team Activity/rexburg.js
@@ -1,15 +1,47 @@
-var today = new Date();
-var time = today.getHours();
-console.log(time);
-if(time <= 6 && time >= 18){document.body.style.backgroundImage = "url('3.jpg')"}
-else {
-   document.body.style.backgroundImage = "url('4.jpg')"
-}
-
 const url =
    "https://api.openweathermap.org/data/2.5/forecast?id=5605242&units=imperial&appid=744fe90a723fbf1d7ad8053159c6a1d5";
 
 const dofW = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]; 
+
+
+function cDay(weatherObjct) {
+   let weatherData = weatherObjct.list[0];
+
+   let dt = new Date(weatherData.dt_txt);
+   let cDay = document.createElement('h3');
+   cDay.textContent = dofW[dt.getDay()];
+   
+
+   let cTemp = document.createElement("p");
+   cTemp.textContent = `Air Temprature: ${weatherData.main.temp} °F`;
+
+   let cWeather = document.createElement("img");
+   let imagesrc =
+      "https://openweathermap.org/img/w/" +
+      weatherData.weather[0].icon +
+      ".png";
+   cWeather.setAttribute("src", imagesrc);
+   cWeather.setAttribute("alt", weatherData.weather[0].description);
+
+   let cFeels = document.createElement("p");
+   cFeels.textContent = `Feels-like: ${weatherData.main.feels_like} °F`;
+
+   let tempHigh = document.createElement("p");
+   tempHigh.textContent = `Maximum Temperature: ${weatherData.main.temp_max} °F`;
+
+   let tempLow = document.createElement("p");
+   tempLow.textContent = `Minimum Temperature: ${weatherData.main.temp_min} °F`;
+
+   let todayElem = document.querySelector("#today");
+
+   todayElem.appendChild(cDay);
+   todayElem.appendChild(cWeather);
+   todayElem.appendChild(cTemp);
+   todayElem.appendChild(cFeels);
+   todayElem.appendChild(tempHigh);
+   todayElem.appendChild(tempLow);
+
+}
 
 fetch(url)
 
@@ -51,43 +83,3 @@ fetch(url)
           }
       }
   }
-
-function cDay(weatherObjct) {
-   let weatherData = weatherObjct.list[0];
-
-   let dt = new Date(weatherData.dt_txt);
-   let cDay = document.createElement('h3');
-   cDay.textContent = dofW[dt.getDay()];
-   
-
-   let cTemp = document.createElement("p");
-   cTemp.textContent = `Air Temprature: ${weatherData.main.temp} °F`;
-
-   let cWeather = document.createElement("img");
-   let imagesrc =
-      "https://openweathermap.org/img/w/" +
-      weatherData.weather[0].icon +
-      ".png";
-   cWeather.setAttribute("src", imagesrc);
-   cWeather.setAttribute("alt", weatherData.weather[0].description);
-
-   let cFeels = document.createElement("p");
-   cFeels.textContent = `Feels-like: ${weatherData.main.feels_like} °F`;
-
-   let tempHigh = document.createElement("p");
-   tempHigh.textContent = `Maximum Temperature: ${weatherData.main.temp_max} °F`;
-
-   let tempLow = document.createElement("p");
-   tempLow.textContent = `Minimum Temperature: ${weatherData.main.temp_min} °F`;
-
-   let todayElem = document.querySelector("#today");
-
-   todayElem.appendChild(cDay);
-   todayElem.appendChild(cWeather);
-   todayElem.appendChild(cTemp);
-   todayElem.appendChild(cFeels);
-   todayElem.appendChild(tempHigh);
-   todayElem.appendChild(tempLow);
-
-}
-
